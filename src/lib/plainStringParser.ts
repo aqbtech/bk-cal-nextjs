bf06570edec19179ed51e692a6b15ef2e20f1f97
Tu Van Nguyen Anh Quan <tvnaquan@gmail.com>
2025-05-14 11:33:02 +0700

    init plain string parse for course info

---
+++ src/lib/plainStringParser.ts
@@ -0,0 +1,135 @@
+// example string 
+// Môn học được chuyển điểm
+// Mã môn học	Tên môn học	Tín chỉ	Điểm thành phần	Điểm số	Điểm chữ	Điểm đạt	Cập nhật
+// LA1003	Anh văn 1	2	--	MT	--		08/11/2024 14:04
+// LA1005	Anh văn 2	2	--	MT	--		13/11/2023 10:50
+// LA1007	Anh văn 3	2	--	MT	--		13/11/2023 10:50
+// LA1009	Anh văn 4	2	--	MT	--		13/11/2023 10:50
+// Số TCTL chung: 8
+// Năm học 2022-2023 - Học kỳ 1
+// Mã môn học	Tên môn học	Tín chỉ	Điểm thành phần	Điểm số	Điểm chữ	Điểm đạt	Cập nhật
+// CO1005	Nhập môn Điện toán	3	Kiểm tra (30%): 9.5
+// Bài tập lớn (30%): 9.5
+// Thí nghiệm (10%): 10
+// Thi (30%): 7.5	9.0	A		28/03/2024 16:21
+// CO1023	Hệ thống số	3	Kiểm tra (20%): 8.5
+// Thí nghiệm (30%): 8
+// Thi (50%): 5.5	6.9	C+		28/03/2024 16:21
+// LA1003	Anh văn 1	2	Kiểm tra (20%): 13
+// Bài tập (20%): 13
+// Bài tập lớn (10%): 13
+// Thi (50%): 13	RT	--	--	28/03/2023 08:37
+// MT1003	Giải tích 1	4	Kiểm tra (25%): 8.5
+// Bài tập (5%): 6
+// Bài tập lớn (20%): 8
+// Thi (50%): 10	9.0	A		28/03/2024 16:21
+// PE1011	Bóng chuyền (Học phần 1)	0	--	8.0	B+		28/03/2023 08:37
+// PH1003	Vật lý 1	4	Kiểm tra (30%): 8
+// Bài tập (10%): 9
+// Bài tập lớn (10%): 8
+// Thi (50%): 8	8.1	B+		28/03/2024 16:21
+// Số tín chỉ đạt/đăng ký học kỳ: 14/16 - Số TCTL chung: 22	ĐTBHK: 3.5	ĐTBTL chung: 3.5
+// Năm học 2022-2023 - Học kỳ 2
+// Mã môn học	Tên môn học	Tín chỉ	Điểm thành phần	Điểm số	Điểm chữ	Điểm đạt	Cập nhật
+// CO1007	Cấu trúc Rời rạc cho Khoa học Máy tính	4	Kiểm tra (30%): 7
+// Bài tập lớn (20%): 10
+// Thi (50%): 5	6.6	C+		04/03/2024 14:23
+// CO1027	Kỹ thuật Lập trình	3	Bài tập lớn (30%): 9
+// Thí nghiệm (30%): 7.5
+// Thi (40%): 6	7.4	B		04/03/2024 14:23
+// MI1003	Giáo dục Quốc phòng	0	--	DT	--		26/06/2023 11:05
+// MT1005	Giải tích 2	4	Kiểm tra (25%): 9.5
+// Bài tập (5%): 8.5
+// Bài tập lớn (20%): 9
+// Thi (50%): 8.5	8.9	A		04/03/2024 14:23
+// MT1007	Đại số Tuyến tính	3	Kiểm tra (25%): 8.5
+// Bài tập (5%): 10
+// Bài tập lớn (20%): 8
+// Thi (50%): 7.5	8.0	B+		04/03/2024 14:23
+// PE1041	Cầu lông (Học phần 2)	0	--	7.0	B		27/06/2023 15:37
+// PH1007	Thí nghiệm Vật lý	1	Thí nghiệm (50%): 9.5
+// Thi (50%): 9.5	9.5	A+		04/03/2024 14:23
+// Số tín chỉ đạt/đăng ký học kỳ: 15/15 - Số TCTL chung: 37	ĐTBHK: 3.3	ĐTBTL chung: 3.4
+// Năm học 2023-2024 - Học kỳ 1
+// Mã môn học	Tên môn học	Tín chỉ	Điểm thành phần	Điểm số	Điểm chữ	Điểm đạt	Cập nhật
+// CO2003	Cấu trúc Dữ liệu và Giải Thuật	4	Kiểm tra (10%): 9
+// Bài tập lớn (30%): 9.5
+// Thí nghiệm (10%): 9
+// Thi (50%): 9	9.2	A		05/03/2024 10:01
+// CO2007	Kiến trúc Máy tính	4	Kiểm tra (20%): 7
+// Bài tập lớn (30%): 10
+// Thí nghiệm (10%): 9
+// Thi (40%): 8.5	8.7	A		11/03/2024 22:42
+// CO2011	Mô hình hóa Toán học	3	Kiểm tra (40%): 7.5
+// Bài tập lớn (20%): 9
+// Thi (40%): 7.5	7.8	B		05/03/2024 10:01
+// SP1031	Triết học Mác - Lênin	3	Bài tập (20%): 8.5
+// Bài tập lớn (30%): 8
+// Thi (50%): 7.5	7.9	B		05/03/2024 10:01
+// Số tín chỉ đạt/đăng ký học kỳ: 14/14 - Số TCTL chung: 51	ĐTBHK: 3.6	ĐTBTL chung: 3.5
+// Năm học 2023-2024 - Học kỳ 2
+// Mã môn học	Tên môn học	Tín chỉ	Điểm thành phần	Điểm số	Điểm chữ	Điểm đạt	Cập nhật
+// CO2017	Hệ điều hành	3	Bài tập lớn (30%): 8
+// Thí nghiệm (20%): 9
+// Thi (50%): 8.5	8.5	A		17/06/2024 09:56
+// CO2039	Lập trình Nâng cao	3	Bài tập (10%): 10
+// Bài tập lớn (30%): 7.5
+// Thi (60%): 8	8.1	B+		01/06/2024 12:38
+// IM1021	Khởi nghiệp	3	Bài tập (10%): 8.5
+// Bài tập lớn (30%): 7.5
+// Thi (60%): 7.5	7.6	B		27/06/2024 14:51
+// MT2013	Xác suất và Thống kê	4	Kiểm tra (25%): 9.5
+// Bài tập lớn (25%): 9
+// Thi (50%): 9	9.1	A		02/06/2024 05:46
+// SP1033	Kinh tế Chính trị Mác - Lênin	2	Bài tập (20%): 7
+// Bài tập lớn (30%): 9
+// Thi (50%): 7	7.6	B		29/05/2024 08:46
+// Số tín chỉ đạt/đăng ký học kỳ: 15/15 - Số TCTL chung: 66	ĐTBHK: 3.6	ĐTBTL chung: 3.5
+// Năm học 2024-2025 - Học kỳ 1
+// Mã môn học	Tên môn học	Tín chỉ	Điểm thành phần	Điểm số	Điểm chữ	Điểm đạt	Cập nhật
+// CH1003	Hóa đại cương	3	Bài tập lớn (20%): 9.5
+// Thí nghiệm (30%): 8.5
+// Thi (50%): 9	9.0	A		07/01/2025 13:51
+// CO2013	Hệ cơ sở Dữ liệu	4	Bài tập (20%): 7.5
+// Thí nghiệm (30%): 9.5
+// Thi (50%): 7	7.9	B		04/02/2025 14:16
+// CO3001	Công nghệ Phần mềm	3	Bài tập (10%): 10
+// Bài tập lớn (40%): 8.5
+// Thi (50%): 10	9.4	A		04/02/2025 14:18
+// CO3093	Mạng máy tính	3	Bài tập lớn (30%): 9.5
+// Thí nghiệm (10%): 10
+// Thi (60%): 9.5	9.6	A+		15/01/2025 15:04
+// CO3103	Đồ án Tổng hợp - hướng Công nghệ Phần mềm	1	Bài tập lớn (50%): 10
+// Thi (50%): 10	10	A+		04/02/2025 14:21
+// SP1007	Pháp luật Việt Nam Đại cương	2	Bài tập (20%): 9.5
+// Bài tập lớn (30%): 7.5
+// Thi (50%): 7	7.7	B		31/12/2024 09:10
+// SP1035	Chủ nghĩa Xã hội Khoa học	2	Bài tập (20%): 9
+// Bài tập lớn (30%): 8
+// Thi (50%): 8.5	8.5	A		31/12/2024 09:01
+// Số tín chỉ đạt/đăng ký học kỳ: 18/18 - Số TCTL chung: 84	ĐTBHK: 3.7	ĐTBTL chung: 3.5
+
+import { RawCourseData } from "./htmlParser";
+interface CourseParse1Line { 
+    // example: PE1041	Cầu lông (Học phần 2)	0	--	7.0	B		27/06/2023 15:37
+    // code: PE[0-9][0-9][0-9][0-9], head of line[0]
+    // name: string, line[1]
+    // credits: number, line[2], default 0
+    // score: number | string, line[4]
+    code: string;
+    name: string;
+    credits: number;
+    score: number | string;
+}
+
+interface CourseParse2Line {
+    // example: 
+    // CO3103	Đồ án Tổng hợp - hướng Công nghệ Phần mềm	1	Bài tập lớn (50%): 10
+    // Thi (50%): 10	10	A+		04/02/2025 14:21
+
+
+    
+}
+export const extractCourseData = (plainStringContent: string): RawCourseData[] => {
+    return [];
+}
